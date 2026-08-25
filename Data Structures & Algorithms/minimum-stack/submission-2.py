9ee2a229373277118b906466e51e91ffce31594d
yarkoody <48298821+yarkoody@users.noreply.github.com>
2026-08-25 10:54:22 +0300

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val,val))
+        else:
+            min_so_far = min(val, self.stack[-1][1])
+            self.stack.append((val,min_so_far))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
